8a81fb0e0e6248d019b85ffde6a516654c583980
Harun Aşık <harun.esad@gmail.com>
2023-04-25 16:13:58 +0300

    test

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -6,19 +6,24 @@ public class Fire : MonoBehaviour
 {
     LineRenderer line;
     ParticleSystem particle;
+    AudioSource fire;
     public Transform target;
+    float time = 0;
     void Start()
     {
         line = GetComponentInChildren<LineRenderer>();
         particle = GetComponentInChildren<ParticleSystem>();
+        fire = GetComponentInChildren<AudioSource>();
     }
     void Update()
     {
         line.SetPosition(0, transform.position);
         line.SetPosition(1, target.position);
-        if (Input.GetKey(KeyCode.R))
+        time += Time.deltaTime;
+        if (Input.GetKey(KeyCode.R) && time < 2)
         {
             particle.Play();
+            fire.Play();
         }
     }
 }
