e13c29ddf7224384bf8744b7975c7083f44ef8c4
Harun Aşık <harun.esad@gmail.com>
2023-04-25 09:39:49 +0300

    test

--- Assets/Scripts/ExampleObject.cs
+++ Assets/Scripts/ExampleObject.cs
@@ -4,30 +4,12 @@ using UnityEngine;
 
 public class ExampleObject : MonoBehaviour
 {
-    [SerializeField] GameObject spawnBullet;
-    Transform spawnPoint;
-    [SerializeField] Transform gunTile;
-    float time;
     void Start()
     {
-        spawnPoint = transform.GetChild(3);
-        time = 0;
+
     }
     void Update()
     {
-        if (time >= 2f)
-        {
-            Spawn();
-            time = 0;
-        }
-        else
-        {
-            time += Time.deltaTime;
-        }
-    }
-    void Spawn()
-    {
-        GameObject amount = Instantiate(spawnBullet, spawnPoint.position, Quaternion.identity, gunTile);
-        Destroy(amount, 2.5f);
+        transform.Rotate(Vector3.right * 100 * Time.deltaTime);
     }
 }
