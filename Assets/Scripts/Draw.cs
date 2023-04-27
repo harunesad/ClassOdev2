6a376d8e45eab01e4a2ff2a5b8425dd693b582ac
Harun Aşık <harun.esad@gmail.com>
2023-04-27 11:20:46 +0300

    draw fixed

--- Assets/Scripts/Draw.cs
+++ Assets/Scripts/Draw.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Draw : MonoBehaviour
 {
     LineRenderer line;
+    EdgeCollider2D edge;
+    Vector3 linePoint;
     private void Awake()
     {
         line = GetComponent<LineRenderer>();
+        edge = GetComponent<EdgeCollider2D>();
     }
     void Start()
     {
-
+        
     }
     void Update()
     {
@@ -35,6 +38,22 @@ public class Draw : MonoBehaviour
                 {
                     line.SetVertexCount(line.positionCount + 1);
                     line.SetPosition(line.positionCount - 1, hit.point + Vector3.up);
+
+                    List<Vector2> points = new List<Vector2>();
+                    for (int i = 0; i < line.positionCount; i++)
+                    {
+                        if (i == 0)
+                        {
+                            linePoint = Vector3.zero;
+                        }
+                        else
+                        {
+                            linePoint = line.GetPosition(i) - line.GetPosition(0);
+                            //linePoint = new Vector3(linePoint.x + points[0].x, linePoint.y, linePoint.z + points[0].y);
+                        }
+                        points.Add(new Vector2(linePoint.x, linePoint.y));
+                    }
+                    edge.SetPoints(points);
                 }
             }
         }
